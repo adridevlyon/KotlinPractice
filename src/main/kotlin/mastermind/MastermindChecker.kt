fba1ff523ccecfc103e7317e18f82ab89865697f
adridevparis <>
2021-04-09 16:01:23 +0200

    [mastermind] rename variables and function

--- src/main/kotlin/mastermind/MastermindChecker.kt
+++ src/main/kotlin/mastermind/MastermindChecker.kt
@@ -5,46 +5,46 @@ import kotlin.math.max
 class MastermindChecker {
     fun check(attempt: String, solution: String): MastermindState {
         val mastermindState = MastermindState(solution = solution)
-        for ((index, item) in attempt.withIndex()) {
-            mastermindState.process(index, item)
+        for ((index, char) in attempt.withIndex()) {
+            mastermindState.process(index, char)
         }
         return mastermindState
     }
 }
 
 class MastermindState(val solution: String, var placed: Int = 0, var present: Int = 0) {
-    fun process(index: Int, item: Char) {
-        if (isPlaced(index, item)) {
-            addPlaced(item)
-        } else if (isPresent(item)) {
-            addPresent(item)
+    fun process(index: Int, char: Char) {
+        if (isPlaced(index, char)) {
+            addPlaced(char)
+        } else if (isPresent(char)) {
+            addPresent(char)
         }
     }
 
-    private fun isPlaced(index: Int, item: Char): Boolean {
-        return solution[index] == item
+    private fun isPlaced(index: Int, char: Char): Boolean {
+        return solution[index] == char
     }
 
-    private fun addPlaced(item: Char) {
+    private fun addPlaced(char: Char) {
         placed++
-        removeFromItemsByType(item)
+        removeCharFromCounts(char)
     }
 
-    private fun isPresent(item: Char): Boolean {
-        return itemsByType.getOrDefault(item, 0) > 0
+    private fun isPresent(char: Char): Boolean {
+        return solutionCountsByChar.getOrDefault(char, 0) > 0
     }
 
-    private fun addPresent(item: Char) {
+    private fun addPresent(char: Char) {
         present++
-        removeFromItemsByType(item)
+        removeCharFromCounts(char)
     }
 
-    private fun removeFromItemsByType(item: Char) {
-        itemsByType[item] = max(0, itemsByType.getOrDefault(item, 0) - 1)
+    private fun removeCharFromCounts(char: Char) {
+        solutionCountsByChar[char] = max(0, solutionCountsByChar.getOrDefault(char, 0) - 1)
     }
 
-    private val itemsByType: MutableMap<Char, Int> = computeItemsByType(solution)
-    private fun computeItemsByType(solution: String): MutableMap<Char, Int> {
+    private val solutionCountsByChar: MutableMap<Char, Int> = computeCountsByChar(solution)
+    private fun computeCountsByChar(solution: String): MutableMap<Char, Int> {
         return mutableMapOf(
             *solution.groupBy { it }
                 .map { (char, chars) -> char to chars.size }
